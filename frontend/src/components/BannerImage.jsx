d0d63dcfd52a619bf996d5ca6e53d09f44b0db67
siva6168 <60339869+siva6168@users.noreply.github.com>
2023-02-18 23:40:55 +0530

    Banner

--- frontend/src/components/BannerImage.jsx
+++ frontend/src/components/BannerImage.jsx
@@ -4,7 +4,7 @@ const BannerImage = ({name}) => {
   return (
     <div><div className="p-5 text-center bg-image" style={{
         backgroundImage: "url(https://images.pexels.com/photos/693776/pexels-photo-693776.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
-        height:"75vh"}}>
+        height:"85vh"}}>
       <div className="mask" style={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}>
         <div className="d-flex justify-content-center align-items-center h-100">
           <div className="text-white">
