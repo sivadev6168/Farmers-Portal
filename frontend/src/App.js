c89e0d539403961413aa6b1be8920ec3a4d4f114
Aneesh KS <aneesh.ks@loremine.com>
2023-03-05 21:54:56 +0530

    added whatsapp msg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Routing from './components/Routing';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 
 function App() {
-    return ( <
+    return ( 
+        <>
+        <FloatingWhatsApp/>
+        <
         Routing / >
+        </>
     );
 }
 
